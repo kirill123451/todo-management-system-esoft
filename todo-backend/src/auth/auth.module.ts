23cd28553d12f510630a5f8ddb09b330ad6642b4
kirill <prostoskvasil@gmail.com>
2026-05-23 17:15:56 +0500

    chore: update localtunnel backend configuration

--- todo-backend/src/auth/auth.module.ts
+++ todo-backend/src/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersModule } from '../users/users.module'; // Исправил путь на относительный для стабильности
+import { UsersModule } from '../users/users.module'; 
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
@@ -11,7 +11,6 @@ import { JwtStrategy } from './jwt.strategy';
     UsersModule,
     PassportModule,
     JwtModule.register({
-      // ИСПРАВЛЕНО: Ключ теперь совпадает со стратегией
       secret: 'SUPER_SECRET_KEY_123', 
       signOptions: { expiresIn: '1d' }
     })
